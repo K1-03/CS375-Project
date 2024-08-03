aa349ec610d08ff8fb8b980871e8a66d9cfcc92b
smm834 <smm834@drexel.edu>
2024-08-03 19:42:29 -0400

    Video upload functionality in server.

--- Current/server.js
+++ Current/server.js
@@ -1,7 +1,30 @@
 let express = require('express');
 let path = require('path');
+const fs = require('fs');
+const multer = require('multer');
 let app = express();
 let port = 3000;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'videos'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1804);
+    cb(null, file.fieldname + '_' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+if (!fs.existsSync('videos')) {
+  fs.mkdir('videos', { recursive: true }, (err) => {
+    if (err) {
+      console.error('Error creating "videos" directory:', err);
+    } 
+  });
+} 
+
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,6 +40,10 @@ app.get('/video', (req, res) => {
 
 app.get('/account', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'account.html'));
+});
+
+app.post("/upload", upload.single('v'), (req, res) =>{
+  res.send('Video Uploaded.');
 });
 
 app.listen(port, () => {
