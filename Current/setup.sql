17c34f3f3e074376c7440ab296e3c341af2b66e1
Robel Eyob <re368@drexel.edu>
2024-08-04 19:35:43 -0400

    KAN-3 Added env.json and setup.sql

---
+++ Current/setup.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE current;
+\c current
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL
+);
