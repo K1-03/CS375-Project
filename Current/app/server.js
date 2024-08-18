5b5c07336b5e0f487081b8f7b25efee985eb0e86
Robel Eyob <re368@drexel.edu>
2024-08-17 22:09:41 -0400

    fixed merge issue and everything is updated

---
+++ Current/app/server.js
@@ -0,0 +1,184 @@
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs'); // just changed this to bcryptjs instead of bcrypt
+const cookieParser = require('cookie-parser');
+const { Pool } = require('pg');
+const env = require('dotenv').config();
+let host;
+let databaseConfig;
+
+if (process.env.NODE_ENV == "production") {
+
+	host = "0.0.0.0";
+	databaseConfig = { connectionString: process.env.DATABASE_URL };
+
+} else {
+
+	host = "localhost";
+	let { PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT } = process.env;
+	databaseConfig = { PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT };
+
+}
+
+let pool = new Pool(databaseConfig);
+
+pool.connect().then(() => {
+  console.log("Connected to database");
+});
+
+let app = express();
+let port = 8080;
+const fs = require('fs');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'videos'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1804);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+if (!fs.existsSync('videos')) {
+  fs.mkdir('videos', { recursive: true }, (err) => {
+    if (err) {
+      console.error('Error creating "videos" directory:', err);
+    } 
+  });
+} 
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTML files
+app.get('/', (req, res) => {
+  const userInfo = req.cookies.userInfo;
+  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
+app.get('/video', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'video.html'));
+});
+
+app.get('/account', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'account.html'));
+});
+
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'upload.html'));
+});
+
+app.post("/upload", upload.single('v'), (req, res) =>{
+  let videoId = req.file.filename.split(".")[0];
+  let title = req.query.title;
+  let description = req.query.description;
+
+  pool.query("INSERT INTO video_information (vid, title, description, tags) VALUES ($1, $2, $3, $4)", [videoId, title, description, "temp"]);
+
+  res.json({ message: 'Video uploaded successfully.', redirectUrl: '/account' });
+});
+
+app.get('/signin', (req, res) => {
+  res.sendFile(path.join(__dirname,'public', 'html', 'signin.html'));
+});
+
+app.get ('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'signup.html'));
+});
+
+app.post('/signin', (req, res) => {
+  const { emailOrUsername, password } = req.body;
+
+  pool.connect((err, client, release) => {
+    if (err) return res.status(500).json({ message: 'Server error' });
+
+    const isEmail = emailOrUsername.includes('@');
+    const query = isEmail ?
+      'SELECT * FROM users WHERE email = $1' :
+      'SELECT * FROM users WHERE username = $1';
+
+      const value = isEmail ? emailOrUsername.toLowerCase : emailOrUsername;
+    
+    client.query(query, [value], (err, result) => {
+      release();
+      if (err) return res.status(500).json({ message: 'Server error' });
+      
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        bcrypt.compare(password, user.password, (err, isValid) => {
+          if (err) return res.status(500).json({ message: 'Server error' });
+          if (isValid) {
+            res.cookie('userInfo', {
+              email: user.email,
+              firstName: user.first_name,
+              lastName: user.last_name,
+              username: user.username,
+              id: user.id
+            }, { httpOnly: true });
+            res.json({ message: 'Sign in successful.', redirect: '/' });
+          } else {
+            res.json({ message: 'Invalid email/username or password.' });
+          }
+        });
+      } else {
+        res.json({ message: 'Invalid email/username or password.' });
+      }
+    });
+  });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, lastName, username } = req.body;
+
+  const usrenameRegex = /^[a-zA-Z0-9]+$/;
+
+  const lowerCaseEmail = email.toLowerCase();
+
+  if (!usrenameRegex.test(username)) {
+    return res.status(400).json({ message: 'Username can only contain letters and numbers.' });
+  }
+
+  pool.connect((err, client, release) => {
+    if (err) return res.status(500).json({ message: 'Server error' });
+
+    client.query('SELECT * FROM users WHERE email = $1 OR username = $2', [lowerCaseEmail, username], (err, result) => {
+      if (err) {
+        release();
+        return res.status(500).json({ message: 'Server error' });
+      }
+
+      if (result.rows.length > 0) {
+        release();
+        return res.status(409).json({ message: 'Account with that email or username already exists.' });
+      }
+      
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+          release();
+          return res.status(500).json({ message: 'Server error' });
+        }
+        client.query(
+          'INSERT INTO users (email, password, first_name, last_name, username) VALUES ($1, $2, $3, $4, $5) RETURNING *', 
+          [lowerCaseEmail, hashedPassword, firstName, lastName, username],
+          (err, result) => {
+            release();
+            if (err) return res.status(500).json({ message: 'Server error' });
+            
+            res.status(201).json({ message: 'Account created successfully.' });
+          }
+        );
+      });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
