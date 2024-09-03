6040e2e464b9289655e1e4e0e37e7294c6666c92
smm834 <smm834@drexel.edu>
2024-09-03 10:06:39 -0400

    Username and PFP in comments link to their channel page now.

--- Current/app/public/javascript/video.js
+++ Current/app/public/javascript/video.js
@@ -63,15 +63,19 @@ commentButton.addEventListener('click', async () =>{
   profileImage.setAttribute("alt", `${data.userInfo.username}'s profile picture`);
   profileImage.setAttribute("class", "comment-avatar");
 
-  let userName = document.createElement("p");
+  let userName = document.createElement("a");
   let commentBody = document.createElement("p");
+  let channelLink = document.createElement("a");
   
   userName.textContent = `@${data.userInfo.username}`;
   commentBody.textContent = commentInput.value;
 
-  userName.setAttribute("style", "font-weight: bold; display: inline;");
+  userName.setAttribute("class", "username-channel-link");
+  userName.setAttribute("href", `/${data.userInfo.username}`);
+  channelLink.setAttribute("href", `/${data.userInfo.username}`);
 
-  commentElement.appendChild(profileImage);
+  channelLink.appendChild(profileImage);
+  commentElement.appendChild(channelLink);
   commentElement.appendChild(userName);
   commentElement.appendChild(commentBody);
 
@@ -162,15 +166,19 @@ document.addEventListener('DOMContentLoaded', () => {
                     profileImage.setAttribute("alt", `${userInfo.username}'s profile picture`);
                     profileImage.setAttribute("class", "comment-avatar");
 
-                    let userName = document.createElement("p");
+                    let userName = document.createElement("a");
                     let commentBody = document.createElement("p");
+                    let channelLink = document.createElement("a");
   
                     userName.textContent = `@${userInfo.username}`;
                     commentBody.textContent = `${comment.content}`;
 
-                    userName.setAttribute("style", "font-weight: bold; display: inline;");
+                    userName.setAttribute("class", "username-channel-link");
+                    userName.setAttribute("href", `/${userInfo.username}`);
+                    channelLink.setAttribute("href", `/${userInfo.username}`);
 
-                    commentElement.appendChild(profileImage);
+                    channelLink.appendChild(profileImage);
+                    commentElement.appendChild(channelLink);
                     commentElement.appendChild(userName);
                     commentElement.appendChild(commentBody);
 
